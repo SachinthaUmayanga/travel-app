cfeb71c86e91ef6c6c644115c02aecd01723fd16
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2025-01-21 20:41:29 +0530

    Review section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import Destination from '@/components/Home/Destination/Destination'
 import Home from '@/components/Home/Home'
-import Hotel from '@/components/Home/Hotel/Hotel'
-import WhyChoose from '@/components/Home/WhyChoose/WhyChoose'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       <Home />
-      <Destination />
-      <Hotel />
-      <WhyChoose />
+      
     </div>
   )
 }
